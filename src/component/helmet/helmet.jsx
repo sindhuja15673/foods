e33c1e809599c18c1e870fe186150365368ab27b
something-man <minnalkodi735@gmail.com>
2024-12-24 19:08:24 +0530

    third

--- src/component/helmet/helmet.jsx
+++ src/component/helmet/helmet.jsx
@@ -1,9 +1,33 @@
-// src/components/HelmetComponent.js
+// // src/components/HelmetComponent.js
+
+// import React from 'react';
+// import { Helmet } from 'react-helmet';
+
+// const HelmetComponent = ({ title, description, keywords, ogTitle, ogDescription, ogUrl, ogImage, ogImageAlt, ogImageWidth, ogImageHeight }) => {
+//   return (
+//     <Helmet>
+//       <title>{title}</title>
+//       <meta name="description" content={description} />
+//       <meta name="keywords" content={keywords} />
+//       <meta property="og:title" content={ogTitle} />
+//       <meta property="og:description" content={ogDescription} />
+//       <meta property="og:type" content="website" />
+//       <meta property="og:url" content={ogUrl} />
+//       <meta property="og:image" content={ogImage} />
+//       <meta property="og:image:alt" content={ogImageAlt} />
+//       <meta property='og:image:width' content={ogImageWidth} />
+//       <meta property='og:image:height' content={ogImageHeight} />
+//     </Helmet>
+//   );
+// };
+
+// export default HelmetComponent;
+// src/components/SEOHelmet.js
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const HelmetComponent = ({ title, description, keywords, ogTitle, ogDescription, ogUrl, ogImage, ogImageAlt, ogImageWidth, ogImageHeight }) => {
+const SEOHelmet = ({ title, description, keywords, ogTitle, ogDescription, ogUrl, ogImage, ogImageAlt, ogImageWidth, ogImageHeight }) => {
   return (
     <Helmet>
       <title>{title}</title>
@@ -17,8 +41,11 @@ const HelmetComponent = ({ title, description, keywords, ogTitle, ogDescription,
       <meta property="og:image:alt" content={ogImageAlt} />
       <meta property='og:image:width' content={ogImageWidth} />
       <meta property='og:image:height' content={ogImageHeight} />
+      <script type="application/ld+json">
+        {JSON.stringify(generateJsonLd())}
+      </script>
     </Helmet>
   );
 };
 
-export default HelmetComponent;
+export default SEOHelmet;
--- src/component/menu/menu.js
+++ src/component/menu/menu.js
@@ -1,3 +1,141 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import ScrollableSection from './scrollbar';
+// import Popup from './popup';
+// import SkeletonLoading from '../skeletonLoader';
+// import Skeleton from 'react-loading-skeleton';
+// import 'react-loading-skeleton/dist/skeleton.css';
+// import { Helmet } from 'react-helmet';
+// import HelmetComponent from '../helmet/helmet';
+
+// export default function Menu() {
+//   const [foodItems, setFoodItems] = useState([]);
+//   const [showPopup, setShowPopup] = useState(false);
+//   const [selectedFood, setSelectedFood] = useState(null);
+//   const [category, setCategory] = useState([])
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       try {
+//         const res = await axios.get('http://localhost:5000/api/products');
+//         setFoodItems(res.data);
+//         setLoading(false); 
+//         const uniqueCategories = [...new Set(res.data.map(item => item.category))];
+//         setCategory(uniqueCategories)
+//       } catch (error) {
+//         console.error('Error fetching products:', error);
+//         setLoading(false); 
+//       }
+//     };
+
+
+//     const timer = setTimeout(() => {
+//       fetchProducts();
+//     }, 1000);
+
+//     // Cleanup timer
+//     return () => clearTimeout(timer);
+//   }, []);
+
+  
+
+//   const handleAddClick = (food) => {
+//     setSelectedFood(food);
+//     setShowPopup(true);
+//   };
+
+//   const handleAddToCart = () => {
+//     setShowPopup(false);
+//     toast.success('Items added to cart!');
+//   };
+//   const generateJsonLd = () => {
+//     return {
+//       "@context": "https://schema.org",
+//       "@type": "Product",
+//       "name": foodItems.map(food => food.name).join(', '),
+//       "image": foodItems.map(food => `${process.env.REACT_APP_API_BASE_URL}/images/${food.img}`).join(', '),
+//       "description": "Food items available for order on our menu",
+//       "sku": foodItems.map(food => food.id).join(', '),
+//       "category": category.join(', '),
+//       "offers": {
+//         "@type": "Offer",
+//         "priceCurrency": "USD",
+//         "price": foodItems.map(food => food.price).join(', ')
+//       }
+//     };
+//   };
+//   return (
+//     <div>
+//        <HelmetComponent
+//         title="Our Menu - Best Food Delivered to Your Door"
+//         ogImage="https://foods-1.onrender.com/assets/pizza.jpg"
+//         ogImageAlt="Delicious food banner"
+//         ogImageWidth="1200"
+//         description="Explore our delicious food menu, including vegetarian, non-vegetarian, and snacks. Order your favorite meals now!"
+//         keywords="menu, food, restaurant, order food online, delicious food, vegetarian food, non-vegetarian food, snacks" 
+//         ogTitle="Our Food Menu"
+//         ogDescription="Browse our wide selection of food items and order online. Fresh, tasty, and delivered to your door!"
+//         ogUrl="http://192.168.0.107:3000"
+        
+//         ogImageHeight="630"
+//       />
+      
+//       {showPopup && (
+//         <Popup
+//           food={selectedFood}
+//           foodItems={foodItems}
+//           onClose={() => setShowPopup(false)}
+//           onAddToCart={handleAddToCart}
+//         />
+//       )}
+//       <ToastContainer />
+//       {loading ? (
+//         <>
+//         <Skeleton height={50} width={250} style={{ marginLeft: 10, marginTop: 10 }} />
+//         <div className="food-item-skeleton-container">
+//           <SkeletonLoading />
+//           <SkeletonLoading />
+//           <SkeletonLoading />
+//           <SkeletonLoading />
+//           <SkeletonLoading />
+//         </div>
+//         <Skeleton height={50} width={250} style={{ marginLeft: 10, marginTop: 10 }} />
+//          <div className="food-item-skeleton-container">
+//          <SkeletonLoading />
+//          <SkeletonLoading />
+//          <SkeletonLoading />
+//          <SkeletonLoading />
+//          <SkeletonLoading />
+        
+//        </div>
+//        <Skeleton height={50} width={250} style={{ marginLeft: 10, marginTop: 10 }} />
+//         <div className="food-item-skeleton-container">
+//         <SkeletonLoading />
+//         <SkeletonLoading />
+//         <SkeletonLoading />
+//         <SkeletonLoading />
+//         <SkeletonLoading />
+       
+//       </div>
+//       </>
+//       ) : (
+//       category.map((category)=>{
+//         const foodCategory = foodItems.filter(food => food.category === category);
+//         return(
+//           <ScrollableSection key={category} title={`${category.charAt(0).toUpperCase() + category.slice(1)} Foods`} foods={foodCategory} onAddClick={handleAddClick} />
+//         )
+//       })
+//     )}
+    
+//     </div>
+//   );
+// }
+
+// src/components/Menu.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
@@ -7,8 +145,7 @@ import Popup from './popup';
 import SkeletonLoading from '../skeletonLoader';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
-import { Helmet } from 'react-helmet';
-import HelmetComponent from '../helmet/helmet';
+import SEOHelmet from '../helmet/SEOHelmet';
 
 export default function Menu() {
   const [foodItems, setFoodItems] = useState([]);
@@ -20,27 +157,21 @@ export default function Menu() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await axios.get('https://foods-6s5p.onrender.com/api/products');
+        const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/products`);
         setFoodItems(res.data);
         setLoading(false); 
         const uniqueCategories = [...new Set(res.data.map(item => item.category))];
         setCategory(uniqueCategories)
       } catch (error) {
         console.error('Error fetching products:', error);
+        toast.error('Failed to load food items, please try again later.');
         setLoading(false); 
       }
     };
 
-
-    const timer = setTimeout(() => {
-      fetchProducts();
-    }, 1000);
-
-    // Cleanup timer
+    const timer = setTimeout(fetchProducts, 1000);
     return () => clearTimeout(timer);
   }, []);
-
-  
 
   const handleAddClick = (food) => {
     setSelectedFood(food);
@@ -51,6 +182,7 @@ export default function Menu() {
     setShowPopup(false);
     toast.success('Items added to cart!');
   };
+
   const generateJsonLd = () => {
     return {
       "@context": "https://schema.org",
@@ -59,38 +191,54 @@ export default function Menu() {
       "image": foodItems.map(food => `${process.env.REACT_APP_API_BASE_URL}/images/${food.img}`).join(', '),
       "description": "Food items available for order on our menu",
       "sku": foodItems.map(food => food.id).join(', '),
-      "category": category.join(', '),
+      "mpn": "925872",
+      "brand": {
+        "@type": "Brand",
+        "name": "Food Delivery"
+      },
+      "review": {
+        "@type": "Review",
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "4",
+          "bestRating": "5"
+        },
+        "author": {
+          "@type": "Person",
+          "name": "Food Critic"
+        }
+      },
       "offers": {
         "@type": "Offer",
+        "url": "https://your-production-url.com",
         "priceCurrency": "USD",
-        "price": foodItems.map(food => food.price).join(', ')
+        "price": foodItems.map(food => food.price).join(', '),
+        "priceValidUntil": "2023-11-05",
+        "itemCondition": "https://schema.org/UsedCondition",
+        "availability": "https://schema.org/InStock",
+        "seller": {
+          "@type": "Organization",
+          "name": "Food Delivery"
+        }
       }
     };
   };
+
   return (
     <div>
-       <HelmetComponent
-        title="Our Menu - Best Food Delivered to Your Door"
-        description="Explore our delicious food menu, including vegetarian, non-vegetarian, and snacks. Order your favorite meals now!"
-        keywords="menu, food, restaurant, order food online, delicious food, vegetarian food, non-vegetarian food, snacks" 
-        ogTitle="Our Food Menu"
-        ogDescription="Browse our wide selection of food items and order online. Fresh, tasty, and delivered to your door!"
-        ogUrl="http://192.168.0.107:3000"
-        ogImage="https://foods-1.onrender.com/assets/pizza.jpg"
-        ogImageAlt="Delicious food banner"
+      <SEOHelmet 
+        title="Food Delivery"
+        description="We are providing the best foods"
+        keywords="food, delivery, order food, restaurant, healthy foods"
+        ogTitle="Food Delivery"
+        ogDescription="We are providing the best foods"
+        ogUrl="https://your-production-url.com"
+        ogImage="%PUBLIC_URL%/logo.png"
+        ogImageAlt="Food Delivery Logo"
         ogImageWidth="1200"
         ogImageHeight="630"
       />
       
-      {showPopup && (
-        <Popup
-          food={selectedFood}
-          foodItems={foodItems}
-          onClose={() => setShowPopup(false)}
-          onAddToCart={handleAddToCart}
-        />
-      )}
-      <ToastContainer />
       {loading ? (
         <>
         <Skeleton height={50} width={250} style={{ marginLeft: 10, marginTop: 10 }} />
@@ -101,36 +249,33 @@ export default function Menu() {
           <SkeletonLoading />
           <SkeletonLoading />
         </div>
-        <Skeleton height={50} width={250} style={{ marginLeft: 10, marginTop: 10 }} />
-         <div className="food-item-skeleton-container">
-         <SkeletonLoading />
-         <SkeletonLoading />
-         <SkeletonLoading />
-         <SkeletonLoading />
-         <SkeletonLoading />
-        
-       </div>
-       <Skeleton height={50} width={250} style={{ marginLeft: 10, marginTop: 10 }} />
-        <div className="food-item-skeleton-container">
-        <SkeletonLoading />
-        <SkeletonLoading />
-        <SkeletonLoading />
-        <SkeletonLoading />
-        <SkeletonLoading />
-       
-      </div>
-      </>
+        </>
       ) : (
-      category.map((category)=>{
-        const foodCategory = foodItems.filter(food => food.category === category);
-        return(
-          <ScrollableSection key={category} title={`${category.charAt(0).toUpperCase() + category.slice(1)} Foods`} foods={foodCategory} onAddClick={handleAddClick} />
-        )
-      })
-    )}
-    
+        <>
+        <h1>Food Items</h1>
+        <div>
+          <h2>Categories</h2>
+          <ul>
+            {category.map((categoryItem, index) => (
+              <li key={index}>{categoryItem}</li>
+            ))}
+          </ul>
+        </div>
+        </>
+      )}
+      {showPopup && (
+        <Popup
+          food={selectedFood}
+          foodItems={foodItems}
+          onClose={() => setShowPopup(false)}
+          onAddToCart={handleAddToCart}
+        />
+      )}
+      <ToastContainer />
+      {!loading && (
+        <ScrollableSection title="Delicious Food" foods={foodItems} onAddClick={handleAddClick} />
+      )}
     </div>
   );
 }
-
 
