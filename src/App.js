7a610ecea3e0cc12b21c60f1f31a732ed1b78aff
sindhuja15673 <132433742+sindhuja15673@users.noreply.github.com>
2024-12-24 14:07:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ function App() {
         ogTitle="Best Place to Order Food Online"
         ogDescription="Explore our wide variety of delicious food items available for online ordering. Get your favorite meals delivered to your doorstep!"
         ogUrl="http://192.168.43.46:3000"
-        ogImage="https://i.imgur.com/lQAb1MH.png"
-        ogImageAlt="Delicious food banner"
-        ogImageWidth="300"
-        ogImageHeight="200"
+        // ogImage="https://i.imgur.com/lQAb1MH.png"
+        // ogImageAlt="Delicious food banner"
+        // ogImageWidth="300"
+        // ogImageHeight="200"
       />
       <Provider store={store}>
     <Router>
