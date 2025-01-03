cee3506cd2c00133951ae50aa64cfd63bc5bae1e
something-man <minnalkodi735@gmail.com>
2025-01-03 15:26:05 +0530

    3

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Menu from './component/menu/menu';
 import Cart from './component/cart/cart';
 import { Provider } from 'react-redux';
 import FoodDetail from './component/foodDetails/foodDetails';
-
+import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import store from './saga/store';
 function App() {
   return (
     <div className='app'>
-
+<HelmetProvider>
       <Provider store={store}>
     <Router>
       <Header/>
@@ -28,6 +28,7 @@ function App() {
       <Footer/>
    </Router>
    </Provider>
+   </HelmetProvider>
     </div>
   );
 }
--- src/home.jsx
+++ src/home.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import Baner from './component/baner/baner';
 import Items from './component/fooditems/items';
 import HelmetComponent from './component/helmet/helmet';
+import { Helmet } from 'react-helmet-async';
 export default function home() {
   return (
     <div>
-
-<HelmetComponent
+ <Helmet>
+      <title>this is home page</title>
+      <meta name="description" content='home page'/>
+      <meta property="og:title" content='this is home page' />
+      <meta property="og:description" content='home page' />
+      <meta property="og:image" content="https://res.cloudinary.com/dhevjlnnc/image/upload/v1735892233/dosa1_cvbvnr.jpg" />
+      <meta property="og:url" content="https://foods-1.onrender.com" />
+    </Helmet>
+{/* <HelmetComponent
         title="Welcome to our Restaurant - Best Place to Order Food Online"
         description="Explore our wide variety of delicious food items available for online ordering. Get your favorite meals delivered to your doorstep!"
         keywords="restaurant, food delivery, online ordering, best food, order food online"
@@ -17,7 +25,7 @@ export default function home() {
         ogImageAlt="Delicious food banner"
         ogImageWidth="300"
         ogImageHeight="200"
-      />
+      /> */}
     <Baner/>
     <Items/>
     </div>
